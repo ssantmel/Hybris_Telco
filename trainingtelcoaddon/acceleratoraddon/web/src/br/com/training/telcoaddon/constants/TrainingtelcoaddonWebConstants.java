59b59259b5ac3866e173e51e2a92a0bd3e2938c0
Samira Santos Melo <ssantmel@emeal.nttdata.com>
2022-09-16 13:52:30 -0300

    Primeiro commit

---
+++ trainingtelcoaddon/acceleratoraddon/web/src/br/com/training/telcoaddon/constants/TrainingtelcoaddonWebConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package br.com.training.telcoaddon.constants;
+
+/**
+ * Global class for all Trainingtelcoaddon web constants. You can add global constants for your extension into this class.
+ */
+public final class TrainingtelcoaddonWebConstants // NOSONAR
+{
+	private TrainingtelcoaddonWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ trainingtelcoaddon/src/br/com/training/telcoaddon/constants/TrainingtelcoaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package br.com.training.telcoaddon.constants;
+
+/**
+ * Global class for all Trainingtelcoaddon constants. You can add global constants for your extension into this class.
+ */
+@SuppressWarnings("deprecation")
+public final class TrainingtelcoaddonConstants extends GeneratedTrainingtelcoaddonConstants
+{
+	public static final String EXTENSIONNAME = "trainingtelcoaddon";
+
+	private TrainingtelcoaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
